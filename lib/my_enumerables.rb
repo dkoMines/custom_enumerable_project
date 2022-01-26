f9a9abd97dce4e86c4ded4ea78eed23cc800fcf4
dkoMines <dko@mines.edu>
2022-01-26 12:53:59 -0700

    All tests now pass

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,68 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    (0...size).each { |i|
+      yield self[i], i
+    }
+    self
+  end
+
+  def my_select
+    selected = []
+    (0...size).each { |i|
+      selected << self[i] if yield self[i]
+    }
+    selected
+  end
+
+  def my_all?
+    answer = true
+    (0...size).each { |i|
+      answer = false unless yield self[i]
+    }
+    answer
+  end
+
+  def my_any?
+    answer = false
+    (0...size).each { |i|
+      answer = true if yield self[i]
+    }
+    answer
+  end
+
+  def my_none?
+    answer = true
+    (0...size).each { |i|
+      answer = false if yield self[i]
+    }
+    answer
+  end
+
+  def my_count
+    return size unless block_given?
+    count = 0
+    (0...size).each { |i|
+      count += 1 if yield self[i]
+    }
+    count
+  end
+
+  def my_map
+    mapped_values = []
+    (0...size).each { |i|
+      new_val = yield self[i]
+      mapped_values << new_val
+    }
+    mapped_values
+  end
+
+  def my_inject(total)
+    (0...size).each { |i|
+      total = yield total, self[i]
+    }
+    total
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +71,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    (0...size).each { |i|
+      yield self[i]
+    }
+    self
+  end
 end
